71e7ee17c89d93502519ca57150329639f1d5084
Vector05 <mathurraghav23@gmail.com>
2026-02-12 06:29:02 +0000

    Assignment 12

--- Assignments/Assignment12.c
+++ Assignments/Assignment12.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-void main() {
+#include <stdlib.h>
 
-    // Assignment 12
-    // Program to perform mathematical operations on two numbers
-    // - Smallest Common Divisor (SCD)
-    // - Greatest Common Divisor (GCD)
-
-    // Input
-    int num1, num2, scd, gcd, min_value, max_value, is_scd_found = 0, is_gcd_found = 0;
-    printf("Enter number 1: ");
-    scanf("%d", &num1);
-    printf("Enter number 2: ");
-    scanf("%d", &num2);
-
-    min_value = (num1 < num2) ? num1 : num2;
-    max_value = (num1 > num2) ? num1 : num2;
-
+int scd_calc (int num1, int num2) {
     // Calculate SCD
-    scd = 2;
+    int is_scd_found = 0
+    int min_value = (num1 < num2) ? num1 : num2;
+    int scd = 2;
     while (scd <= min_value) {
         if (num1 % scd == 0 && num2 % scd == 0) {
             is_scd_found = 1;
@@ -28,10 +16,15 @@ void main() {
     if (!is_scd_found) {
         scd = 1; // If no common divisor found, SCD is 1
     }
-    printf("Smallest Common Divisor of %d and %d is %d\n", num1, num2, scd);
 
+    return scd;
+}
+
+int gcd_calc (int num1, int num2) {
     // Calculate GCD
-    gcd = max_value;
+    int is_gcd_found = 0;
+    int max_value = (num1 > num2) ? num1 : num2;
+    int gcd = max_value;
     while (gcd > 1) {
         if (num1 % gcd == 0 && num2 % gcd == 0) {
             is_gcd_found = 1;
@@ -42,6 +35,76 @@ void main() {
     if (!is_gcd_found) {
         gcd = 1; // If no common divisor found, GCD is 1
     }
-    printf("Greatest Common Divisor of %d and %d is %d\n", num1, num2, gcd);
 
+    return gcd;
+}
+
+void main() {
+
+    // Assignment 12
+    // Program to perform mathematical operations on two numbers
+    // - Smallest Common Divisor (SCD)
+    // - Greatest Common Divisor (GCD)
+
+    // Input
+    int num1, num2, choice;
+    printf("Enter number 1: ");
+    scanf("%d", &num1);
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+
+    num1 = mod(abs(num1));
+    num2 = mod(abs(num2));
+
+    printf("Choose Operation:\n1.Calculate SCD\n2.Calculate GCD\n3.Calculate Both SCD and GCD\n");
+    scanf("%d", &choice);
+
+    switch (choice){
+        case 1:
+            int scd_value = scd_calc(num1, num2);
+            printf("Smallest Common Divisor of %d and %d is %d\n", num1, num2, scd_value);
+            break;
+        case 2:
+            int gcd_value = gcd_calc(num1, num2);
+            printf("Greatest Common Divisor of %d and %d is %d\n", num1, num2, gcd_value);
+            break;
+        case 3:
+            int scd_value = scd_calc(num1, num2);
+            printf("Smallest Common Divisor of %d and %d is %d\n", num1, num2, scd_value);
+            int gcd_value = gcd_calc(num1, num2); 
+            printf("Greatest Common Divisor of %d and %d is %d\n", num1, num2, gcd_value);
+            break;
+        default:
+            printf("Invalid Choice\n");
+    }
+/*
+
+    // Implementation using Euclidean algorithm
+
+    // Calculate GCD using Euclidean algorithm
+    for (int i = 2; i <= num1 && i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            printf("GCD of %d and %d using Euclidean algorithm is %d\n", num1, num2, i);
+            break;
+        }
+    }
+    
+    // Calculate SCD using Euclidean algorithm
+    scd = -1;
+    for (int i = 2; i <= num1 && i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            scd = i;
+            break;
+        }
+    }
+    if (scd == -1) {
+            printf("No Common Divisor Found for %d and %d\n", num1, num2);
+        } else {
+            printf("SCD of %d and %d using Euclidean algorithm is %d\n", num1, num2, scd);
+        }
+*/
+    
+    // LCM Calculation using GCD
+    //int lcm = (num1 * num2) / gcd;
+    //printf("LCM of %d and %d is %d\n", num1, num2, lcm);
 }
--- README.md
+++ README.md
@@ -59,9 +59,9 @@ Code 4: Standard Output Configuration with math.h -- (execute in terminal)
                 Modifications : Make the program menu driven
                 Review : Logical Review 
 -Assignment12.c - Program to perform mathematical operations on two numbers
-                Status : Completed
-                Modifications : Use euclidean algorithm for program implementation
-                Note : Update for Program extension to other mathematical operations and better logical development
+                Status : Imcomplete | Program under development for menu driven approach
+                Modifications : Modify euclidean algorithm code
+                Note : Program consists of LCM calculation | Update the program for extension to other mathematical operations
 -Assignment13.c - Program to reverse a number
                 Status : Completed
 </pre>
