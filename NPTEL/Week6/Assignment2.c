7feb30dcf7fa4b022fb2226e23fb2b0d1a21a277
Vector05 <mathurraghav23@gmail.com>
2026-03-21 12:03:54 +0000

    nptel week 6

---
+++ NPTEL/Week6/Assignment2.c
@@ -0,0 +1,27 @@
+// Write a C Program to print the array elements in reverse order (Not reverse sorted order. Just the last element will become first element, second last element will become second element and so on)
+
+#include<stdio.h>
+int main() {
+   int arr[20], i, n;
+ 
+   scanf("%d", &n); /* Accepts the number of elements in the array */
+
+  for (i = 0; i < n; i++) 
+     scanf("%d", &arr[i]); /*Accepts the elements of the array */
+int j, temp;  
+j = i - 1;   // last Element of the array
+i = 0;       // first element of the array
+ 
+   while (i < j) {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;             
+      j--;        
+   }
+for (i = 0; i < n; i++) {
+      printf("%d\n", arr[i]); // For printing the array elements 
+   }
+ 
+   return (0);
+}
